8af7d841bc5f48c7439ca283b2d6e48615d37241
Sushar Hembram <115712646+UNKN0WN006@users.noreply.github.com>
2025-08-20 07:25:55 +0000

    DIfferent file

--- day4_ass4/diff.c
+++ day4_ass4/diff.c
@@ -1,61 +1,49 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<fcntl.h>
-#include<sys/mman.h>
-#include<unistd.h>
-#include<stdint.h>
-#include<inttypes.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <time.h>
 
-#define FILE_SIZE (8L * 1024L * 1024L * 1024L) //8GB
+#define FILE_SIZE (8L * 1024L * 1024L * 1024L) // 8GB
+
 int main() {
-    const char *filename = "bigfile.bin";
-    srand(time(NULL)); // Seeding random number generator
-
-    int fd = open(filename, O_RDWR);
-    if (fd == -1) {
-        perror("Failed to open file");
+    int fd = open("bigfile.bin", O_RDWR);
+    if (fd < 0) {
+        perror("open");
         return 1;
     }
-    // Pre-allocating file space once
+
+    srand(time(0));
+
+    // Make sure file is big enough
     fallocate(fd, 0, 0, FILE_SIZE);
+
     while (1) {
-        // Generating a random offset (F) between 0 and 8GB
-        off_t F = ((off_t)rand() * RAND_MAX + rand()) % FILE_SIZE;
+        off_t pos = (off_t)rand() % FILE_SIZE;
+        unsigned char val = rand() % 256;
 
-        // Generating a random byte value (X) between 0 and 255
-        uint8_t X = rand() % 256;
+        lseek(fd, pos, SEEK_SET);
+        write(fd, &val, 1);
 
-        // Writing the byte X at offset F
-        if (lseek(fd, F, SEEK_SET) == -1) {
-            perror("Failed to seek to offset F");
-            break;
-        }
-        if (write(fd, &X, 1) != 1) {
-            perror("Failed to write byte to file");
-            break;
-        }
-        // Mapping the file into memory using mmap()
-        uint8_t *mapped_file = mmap(NULL, FILE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-        if (mapped_file == MAP_FAILED) {
-            perror("Failed to mmap the file");
+        unsigned char *mem = mmap(NULL, FILE_SIZE, PROT_READ, MAP_SHARED, fd, 0);
+        if (mem == MAP_FAILED) {
+            perror("mmap");
             break;
         }
 
-        // Reading the byte at offset F
-        uint8_t X_prime = mapped_file[F];
-        if (X == X_prime) {
-            printf("Verification successful: byte value at offset 0x%" PRIx64 " is correct.\n", (uint64_t)F);
+        if (mem[pos] == val) {
+            printf("OK at 0x%lx\n", (unsigned long)pos);
         } else {
-            printf("ERROR: Verification failed at offset 0x%" PRIx64 ". Expected byte 0x%02x, but found byte 0x%02x.\n",
-                   (uint64_t)F, X, X_prime);
-            munmap(mapped_file, FILE_SIZE);
+            printf("Mismatch at 0x%lx: wrote 0x%x, read 0x%x\n", (unsigned long)pos, val, mem[pos]);
+            munmap(mem, FILE_SIZE);
             break;
         }
-        // Cleanup
-        munmap(mapped_file, FILE_SIZE);
-        usleep(10000); // slow down loop if needed
+
+        munmap(mem, FILE_SIZE);
+        usleep(10000);
     }
+
     close(fd);
     return 0;
-}
